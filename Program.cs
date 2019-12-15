fc7ca75a3b0bb9a3d9185399e266c8ba90e4c89c
Nathanael Swander <swander926@yahoo.com>
2019-12-14 19:04:23 -0500

    Fixed repeat guess loop. Added exit options

--- Program.cs
+++ Program.cs
@@ -8,26 +8,34 @@ namespace BinaryGuess
     {
       var gamePlay = true;
       var min = 0;
-      var max = 100;
-      var initializer = max / 2;
+      var max = 500000;
+      var initializer = (max) / 2;
 
       int guesses = 0;
+      int lastGuess = 0;
 
       string gameStart;
       string secretNum;
       string userReady;
 
 
-      Console.WriteLine("\nDo you want you mind read? Type P to Play\n");
-      gameStart = Console.ReadLine();
-      gameStart = gameStart.ToUpper();
+      Console.WriteLine("Do you want you mind read? Type P to Play");
+      gameStart = Console.ReadLine().ToUpper();
 
+      while (gameStart != "P")
+      {
+        Console.WriteLine("Error: Do you want you mind read? Type P to Play");
+        gameStart = Console.ReadLine().ToUpper();
+      }
       if (gameStart == "P")
       {
-
-        Console.WriteLine($"\nPick a number {min} - {max} and I will read your mind \n\nType R when Ready\n");
-        userReady = Console.ReadLine();
-        userReady = userReady.ToUpper();
+        Console.WriteLine($"\nPick a number {min} - {max } and I will read your mind \n\nType R when Ready\n");
+        userReady = Console.ReadLine().ToUpper();
+        while (userReady != "R")
+        {
+          Console.WriteLine($"\nPick a number {min} - {max } and I will read your mind \n\nType R when Ready\n");
+          userReady = Console.ReadLine().ToUpper();
+        }
 
         if (userReady == "R")
         {
@@ -37,25 +45,43 @@ namespace BinaryGuess
           {
             Console.WriteLine($"\nIs your number {initializer}?\n\nType H for Higher or L for Lower\n\nWhen your mind is read type W\n");
 
-            secretNum = Console.ReadLine();
-            secretNum = secretNum.ToUpper();
+            secretNum = Console.ReadLine().ToUpper();
 
             if (secretNum == "H")
             {
-              min = initializer;
-              initializer = (min + max) / 2;
-              guesses++;
+              if (initializer == max)
+              {
+                Console.WriteLine("\nCHEATER\n");
+                gamePlay = false;
+              }
+              else if (lastGuess != initializer)
+              {
+                min = initializer;
+                lastGuess = initializer;
+                initializer = (int)Math.Round((min + max) / 2d);
+                guesses++;
+              }
             }
             else if (secretNum == "L")
             {
-              max = initializer;
-              initializer = (min + max) / 2;
-              guesses++;
+              if (initializer == min)
+              {
+                Console.WriteLine("\nCHEATER\n");
+                gamePlay = false;
+
+              }
+              else if (lastGuess != initializer)
+              {
+                max = initializer;
+                lastGuess = initializer;
+                initializer = (int)Math.Round((min + max) / 2d);
+                guesses++;
+              }
             }
             else if (secretNum == "W")
             {
-              gamePlay = false;
               Console.WriteLine($"\nNow it all makes sense\n\nI only used {guesses} hints\n");
+              gamePlay = false;
             }
             else
             {
@@ -63,18 +89,18 @@ namespace BinaryGuess
               gamePlay = false;
             }
           }
-
+          // Console.WriteLine("\nCHEATER\n");
         }
 
         else
         {
-          Console.WriteLine("Your Loss");
+          Console.WriteLine("\nYour Loss\n");
         }
 
       }
       else
       {
-        Console.WriteLine("Your Loss");
+        Console.WriteLine("\nYour Loss\n");
       }
 
 
@@ -82,5 +108,4 @@ namespace BinaryGuess
   }
 }
 
-// i would like to figure out how to make 50 dynamic instead
 // block user from continuing to guess
